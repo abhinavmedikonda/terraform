5f55b27054c530e1c5f0d1ef664205ef0a950a83
abhinav medikonda <abhinavmedikonda@hotmail.com>
2025-05-14 12:05:09 -0400

    ec2 deployment

--- modules/aws/vpc/outputs.tf
+++ modules/aws/vpc/outputs.tf
@@ -3,4 +3,4 @@ output subnet_id {
   value       = aws_subnet.instance_subnet.id
 }
 
-# Necessary to communicate with the root module at <root>/terraform/dev/us-east-1/t2-micro/ec2-deployment/main.tf
+# Necessary to communicate with the root module at <root>/terraform/aws-free/us-east-1/t2-micro/ec2-deployment/main.tf
