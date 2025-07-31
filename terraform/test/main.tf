d0983ba3636999ed88fe9d46d0fd99af35bd64bf
abhinav medikonda <abhinavmedikonda@hotmail.com>
2025-07-31 12:17:29 -0700

    test bucket

---
+++ terraform/test/main.tf
@@ -0,0 +1,12 @@
+provider aws {
+  region = "us-east-1" # Choose a region where the S3 Free Tier applies
+}
+
+resource aws_s3_bucket test_bucket {
+  bucket = "abhi-terraform-test-bucket"
+
+  tags = {
+    Environment = "Test"
+    ManagedBy   = "Terraform"
+  }
+}
