568c9d4fb2e58c2ff13d3262fd609fee29185317
abhinav medikonda <abhinavmedikonda@hotmail.com>
2025-07-31 21:06:02 -0700

    test bucket

--- terraform/test/main.tf
+++ terraform/test/main.tf
@@ -1,12 +1,57 @@
-provider "aws" {
-  region = "us-east-1" # Choose a region where the S3 Free Tier applies
+# Two blocks:
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.37.0"
+    }
+  }
+
+  backend "remote" {
+    # Update to your Terraform Cloud organization
+    organization = "abhinavmedikonda-terraform"
+
+    workspaces {
+      name = "ec2-deployment"
+    }
+  }
 }
 
-resource "aws_s3_bucket" "test_bucket" {
-  bucket = "abhi-terraform-test-bucket"
+provider "aws" {
+  region = local.location
+}
 
-  tags = {
-    Environment = "Test"
-    ManagedBy   = "Terraform"
+locals {
+  # cwd           = reverse(split("/", path.cwd))
+  # instance_type = local.cwd[1]
+  # location      = local.cwd[2]
+  # environment   = local.cwd[3]
+  instance_type = "t2.micro"
+  location      = "us-east-1"
+  environment   = "aws-free"
+}
+
+output "configurations" {
+  value = {
+    path          = path.cwd
+    instance_type = local.instance_type
+    location      = local.location
+    environment   = local.environment
   }
+}
+
+module "instance" {
+
+  source         = "github.com/abhinavmedikonda/terraform//modules/aws/instance?ref=main"
+  instance_type  = local.instance_type
+  instance_count = 2
+  subnet_id      = module.vpc.subnet_id
+}
+
+module "vpc" {
+  source            = "github.com/abhinavmedikonda/terraform//modules/aws/vpc?ref=main"
+  az                = var.az
+  vpc_cidr_block    = var.vpc_cidr_block
+  subnet_cidr_block = var.subnet_cidr_block
 }
