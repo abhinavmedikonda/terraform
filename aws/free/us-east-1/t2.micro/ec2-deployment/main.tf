d151a35cbd0c0a84f6d7dfb3d62f04eda2b6fbfb
abhinav medikonda <abhinavmedikonda@hotmail.com>
2025-08-02 12:02:08 -0700

    test bucket

--- aws/free/us-east-1/t2.micro/ec2-deployment/main.tf
+++ aws/free/us-east-1/t2.micro/ec2-deployment/main.tf
@@ -1,66 +1,66 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = ">= 3.37.0"
-#     }
-#   }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.37.0"
+    }
+  }
 
-#   backend "remote" {
-#     # Update to your Terraform Cloud organization
-#     organization = "abhinavmedikonda-terraform"
+  backend "remote" {
+    # Update to your Terraform Cloud organization
+    organization = "abhinavmedikonda-terraform"
 
-#     workspaces {
-#       name = "ec2-deployment"
-#     }
-#   }
+    workspaces {
+      name = "ec2-deployment"
+    }
+  }
+}
+
+provider "aws" {
+  region = local.location
+}
+
+locals {
+  # cwd           = reverse(split("/", path.cwd))
+  # instance_type = local.cwd[1]
+  # location      = local.cwd[2]
+  # environment   = local.cwd[3]
+  instance_type = "t2.micro"
+  location      = "us-east-1"
+  environment   = "aws-free"
+}
+
+output "configurations" {
+  value = {
+    path          = path.cwd
+    instance_type = local.instance_type
+    location      = local.location
+    environment   = local.environment
+  }
+}
+
+data "terraform_remote_state" "ec2deployment" {
+  backend = "remote"
+  config = {
+    # Update to your Terraform Cloud organization
+    organization = "abhinavmedikonda-terraform"
+    workspaces = {
+      name = "ec2-deployment"
+    }
+  }
+}
+
+module "instance" {
+
+  source         = "github.com/abhinavmedikonda/terraform//modules/aws/instance?ref=main"
+  instance_type  = local.instance_type
+  instance_count = 2
+  subnet_id      = data.terraform_remote_state.ec2deployment.outputs.subnet_id
+}
+
+# module "vpc" {
+#   source            = "github.com/abhinavmedikonda/terraform//modules/aws/vpc?ref=main"
+#   az                = var.az
+#   vpc_cidr_block    = var.vpc_cidr_block
+#   subnet_cidr_block = var.subnet_cidr_block
 # }
-
-# provider "aws" {
-#   region = local.location
-# }
-
-# locals {
-#   # cwd           = reverse(split("/", path.cwd))
-#   # instance_type = local.cwd[1]
-#   # location      = local.cwd[2]
-#   # environment   = local.cwd[3]
-#   instance_type = "t2.micro"
-#   location      = "us-east-1"
-#   environment   = "aws-free"
-# }
-
-# output "configurations" {
-#   value = {
-#     path          = path.cwd
-#     instance_type = local.instance_type
-#     location      = local.location
-#     environment   = local.environment
-#   }
-# }
-
-# data "terraform_remote_state" "ec2deployment" {
-#   backend = "remote"
-#   config = {
-#     # Update to your Terraform Cloud organization
-#     organization = "abhinavmedikonda-terraform"
-#     workspaces = {
-#       name = "ec2-deployment"
-#     }
-#   }
-# }
-
-# module "instance" {
-
-#   source         = "github.com/abhinavmedikonda/terraform//modules/aws/instance?ref=main"
-#   instance_type  = local.instance_type
-#   instance_count = 2
-#   subnet_id      = module.vpc.subnet_id
-# }
-
-# # module "vpc" {
-# #   source            = "github.com/abhinavmedikonda/terraform//modules/aws/vpc?ref=main"
-# #   az                = var.az
-# #   vpc_cidr_block    = var.vpc_cidr_block
-# #   subnet_cidr_block = var.subnet_cidr_block
-# # }
