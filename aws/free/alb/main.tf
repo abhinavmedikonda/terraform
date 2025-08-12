235009fa2555476014918d3640bf315d726b6f6b
abhinav medikonda <abhinavmedikonda@hotmail.com>
2025-08-11 18:09:44 -0700

    + IPv6

--- aws/free/alb/main.tf
+++ aws/free/alb/main.tf
@@ -133,4 +133,20 @@ resource "aws_security_group" "security_group" {
     protocol         = "tcp"
     ipv6_cidr_blocks = ["::/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound IPv4 traffic"
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    ipv6_cidr_blocks = ["::/0"]
+    description      = "Allow all outbound IPv6 traffic"
+  }
 }
